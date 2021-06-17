1d9ea9904da5c7776386098096ae17b79c8d62c5
Mondliwethu Alphonse Hadebe <hmondliwethu@gmail.com>
2021-06-17 23:01:07 +0000

    Print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Return: 0 All the time
+*/
+int main(void)
+{
+printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
