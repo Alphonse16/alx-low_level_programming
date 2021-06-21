d65091c3d433fe7fde43f2b9c9f4754c555e6731
Mondliwethu Alphonse Hadebe <hmondliwethu@gmail.com>
2021-06-20 17:52:38 -0700

    all

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- *main - main block 
- *description : prints all possible combinations of a single digit
- *Return 0
- */
+*main - main block
+*
+*description : prints all possible combinations of a single digit
+*
+*Return always 0
+*/
 int main(void)
 {
 int number;
@@ -12,16 +14,13 @@ for (number = 48; number <= 57; number++)
 {
 putchar(number);
 
-
 if (number == 57)
 {
 break;
 }
 putchar(',');
-}
-
+putchar(' ');
+)
 putchar('\n');
-
 return (0);
-
-}
+)
