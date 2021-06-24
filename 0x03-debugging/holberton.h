c5a4fd151e78b6ceb4be7d871230b0a2cd69e79c
Mondliwethu Alphonse Hadebe <hmondliwethu@gmail.com>
2021-06-24 13:17:54 +0000

    0 main'

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, in year);
+int convert_day(int month, int day);
+void print_times_table(int n);
+
+#endif
+
