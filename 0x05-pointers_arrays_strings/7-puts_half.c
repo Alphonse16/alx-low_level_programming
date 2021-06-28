e1ed1c838fe8b8882bce6df7a0dcb3c903f8ad1a
Mondliwethu Alphonse Hadebe <hmondliwethu@gmail.com>
2021-06-28 20:04:38 +0000

    Reverse strings, Puts half, printing arrays and copying strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: pointer
+ *
+ *Return: void
+ */
+
+void puts_char(char *str);
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++);
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
