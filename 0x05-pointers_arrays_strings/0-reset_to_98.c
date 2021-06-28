af74b0f1a164e9b217b03a682d0935a2c5a4b9d2
Mondliwethu Alphonse Hadebe <hmondliwethu@gmail.com>
2021-06-28 13:42:22 +0000

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/*
+ * reset_to_98 - updates an integer value to 98
+ * @n: A pointer to the int that wil be updated
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
