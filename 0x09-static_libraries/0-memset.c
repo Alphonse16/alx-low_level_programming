b4eae9c8e483cbdface77435900c2d48681db298
Mondliwethu Alphonse Hadebe <hmondliwethu@gmail.com>
2021-07-09 16:20:44 +0000

    Static Library

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,22 @@
+#include "holberton"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: bytes of memory area
+ * Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n--)
+	{
+		*s++ = b;
+	}
+	return (ptr);
+}
+
+
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _islower - returns 1 if c is lowercase, 0 otherwise
+ * @c: integer to be tested
+ * Return: 1 or 0
+ */
+
+int _islower(int 0)
+{
+	return (c >= 97 && c <= 122);
+}
+
