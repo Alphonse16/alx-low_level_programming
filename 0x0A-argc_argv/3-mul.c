5f5d9da947d23268301896b8473152a395436341
Mondliwethu Alphonse Hadebe <hmondliwethu@gmail.com>
2021-07-12 14:16:04 +0000

    Multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - print the result of the multiplication, followed by a new line
@@ -9,14 +10,30 @@
 
 int main(int argc, char const *argv[])
 {
+	int count;
+	int first_num;
+	int second_num;
+	int results;
+
 	(void)argc;
 
-	if (argc != 3)
+	count = 1;
+	if (argc[count] != '\0')
 	{
-		printf("Error\n");
-		return (1);
+		if (argv[count] == NULL || argv[count + 1] == NULL)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			first_num = atoi(argv[count]);
+			second_num = atoi(argv[count + 1]);
+			result = first_num * second_num;
+
+			printf("%d\n", result);
+		}
 	}
-	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
 
