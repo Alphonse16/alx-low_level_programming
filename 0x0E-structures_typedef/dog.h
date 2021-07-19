cbf1c4ea65586adcc4b6780f3b33e542fdc33e06
Mondliwethu Alphonse Hadebe <hmondliwethu@gmail.com>
2021-07-19 20:44:57 +0000

    Dog fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - dog
  * @name: name of the dog
@@ -16,7 +19,10 @@ typedef struct dog
 } dog_t;
 
 int _strlen(char *s);
-void init_dog(struct dog *d, char *name, float age; char *owner);
+void init_dog(struct dog *d,
+char *name,
+float age;
+char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
